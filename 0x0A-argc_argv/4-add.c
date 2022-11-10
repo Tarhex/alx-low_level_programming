8364408936a397b664d7a4e6de59f685351e2e4f
Tarhe Oweh <smartschoolpay@gmail.com>
2022-11-09 20:15:41 -0800

    Arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,64 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
 
 /**
- * _strNumsOnly - checks a string to see if it only
- * contains digits
- * @s: a pointer to an array of strings
- * @n: the number of strings in the array
- * Return: 1 if only digits found, 0 otherwise
- */
-int _strNumsOnly(char *s)
+* main - program that adds sositive numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: Return Zero
+*
+*/
+int main(int argc, char **argv)
 {
-	if (*s == '\0')
-		return (1);
+	int sum, i, j;
 
-	if (!(*s >= '0' && *s <= '9'))
-		return (0);
-
-	return (_strNumsOnly(s + 1));
-}
-
-/**
- * main - adds positive numbers supplied as arguments to the program
- * @argc: counts the number of arguments supplied
- * @argv: makes an array of strings from the arguments supplied
- * Return: 0 for success, 1 for error
- */
-int main(int argc, char *argv[])
-{
-	int idx;
-	int numsOnly;
-	int total;
-
-	/* if no or insufficient umbers are passed in */
-	if (argc <= 2)
+	sum = 0;
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	/* checking strings only contain digits */
-	idx = 1;
-	while (idx < argc)
-	{
-		numsOnly = _strNumsOnly(argv[idx]);
-		if (numsOnly == 0)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		idx++;
+		sum += atoi(argv[i]);
 	}
-
-	idx = 1;
-	total = 0;
-	while (idx < argc)
-	{
-		total = total + atoi(argv[idx]);
-		idx++;
-	}
-	printf("%d\n", total);
+	printf("%d\n", sum);
 	return (0);
 }
